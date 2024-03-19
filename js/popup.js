3c5e088c122b6431c520e1f13e19b44b7b7687f6
MitchellBloch <mitchelljbloch@gmail.com>
2024-03-19 18:34:04 -0400

    Linked html to js and created an message when clicking the help button

--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,14 @@
 // popup.js
-document.getElementById('startTask').addEventListener('click', function () {
+//document.getElementById('startTask').addEventListener('click', function () {
     // Handle START TASK button click
     // Add your logic here
-});
+//});
 
-document.getElementById('recordTask').addEventListener('click', function () {
+//document.getElementById('recordTask').addEventListener('click', function () {
     // Handle RECORD TASK button click
     // Add your logic here
-});
+//});
 
-document.getElementById('help').addEventListener('click', function () {
-    // Handle HELP button click
-    // Add your logic here
+document.getElementById('helpButtonContainer').addEventListener('click', function () {
+    alert('Press the "Record Task" button to record a message for future use. Press the green arrow button on an existing task to listen to an existing task.');
 });
